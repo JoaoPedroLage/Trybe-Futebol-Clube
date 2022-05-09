0652ae7ea4b121dba08128a8c25a92735758c097
JoaoPedroLage <JoaoPedro.Lage@outlook.com>
2022-05-09 00:16:19 -0300

    feat: create a index to export all interfaces

---
+++ app/backend/src/interfaces/index.ts
@@ -0,0 +1,20 @@
+import { IAuthMiddleware, DataToken } from './auth';
+import { ILogin, ILoginController, ILoginService } from './login';
+import { IUser, UserData } from './user';
+import { ITeamService, ITeamController } from './team';
+import { DataMatch, IMatchService, IMatchController } from './match';
+
+export {
+  IAuthMiddleware,
+  DataToken,
+  ILogin,
+  ILoginController,
+  ILoginService,
+  IUser,
+  UserData,
+  ITeamService,
+  ITeamController,
+  DataMatch,
+  IMatchService,
+  IMatchController,
+};
